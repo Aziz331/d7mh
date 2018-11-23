afef1c106571702f74c03b7061d6a6ffbf1c6ddd
Aziz331 <44896829+Aziz331@users.noreply.github.com>
2018-11-23 19:17:39 +0700

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TheKillers !`,"http://twitch.tv/TheKillers")
+client.user.setGame(`#Dragons !`,"http://twitch.tv/TheKillers")
 client.user.setStatus("dnd")
 });
 
